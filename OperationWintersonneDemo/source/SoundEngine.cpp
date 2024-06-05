c640d3cac076545957995a3ed823a7eaee35f936
Benjamin Funke <bjnfne@web.de>
2024-06-05 13:27:04 +0200

    implement findAndSetMatchingNode()

--- OperationWintersonneDemo/source/SoundEngine.cpp
+++ OperationWintersonneDemo/source/SoundEngine.cpp
@@ -1,3 +1,41 @@
+#include <cstdio>
+
+// Function prototypes
+void findAndSetMatchingNode(int **head, int *current, int *end, int *targetValue);
+
+// Function to check if a stream is valid
+bool isStreamValid(int stream) {
+    int *pNode = nullptr;
+    int *pHead = *(int **)(stream + 0x38);
+    
+    // Find and set the matching node
+    findAndSetMatchingNode(&pNode, pHead, pHead, &stack0x00000004);
+    
+    // Check if the node is valid
+    bool isValid = (*pNode == *(int *)(stream + 0x38));
+
+    // Log a message if the stream is not valid
+    if (isValid) {
+        printf("SoundEngine::IsStreamValid stream %x not valid\n", stream);
+    }
+
+    // Return the validity of the stream
+    return !isValid;
+}
+
+
+// Function to find a matching node and set it as the new head of the list
+void findAndSetMatchingNode(int **head, int *current, int *end, int *targetValue) {
+    if (current != end) {
+        do {
+            if (current[2] == *targetValue) {
+                break;
+            }
+            current = (int *)*current;
+        } while (current != end);
+    }
+    *head = current;
+}
 
 
 
