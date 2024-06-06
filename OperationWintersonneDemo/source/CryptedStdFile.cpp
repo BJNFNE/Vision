0965236d70c6d84a69047bb223611e9a0407d3c6
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-06-06 13:02:13 +0200

    add write

--- OperationWintersonneDemo/source/CryptedStdFile.cpp
+++ OperationWintersonneDemo/source/CryptedStdFile.cpp
@@ -5,3 +5,9 @@ void CryptedStdFile::Read() {
   std::cout << "CryptedStdFile::Read not implemented\n" << std::endl;
   return 0;
 }
+
+void CryptedStdFile::Write() {
+
+  std::cout << "CryptedStdFile::Write not implemented\n" << std::endl;
+  return 0;
+}
