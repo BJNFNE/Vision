b594202ca1d3b9e3f4af1b8f00eae8ff99e24ad6
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-06-06 13:11:04 +0200

    addAdventureCameraControl.cpp to Source driectory

---
+++ OperationWintersonneDemo/source/AdventureCameraControl.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void AdventureCameraControl::GetPosition() {
+  std::cout << "AdventureCameraControl::GetPosition not implemented\n" << std::endl;
+  return 0;
+}
+
+void AdventureCameraControl::GetRotation() {
+  std::cout << "AdventureCameraControl::GetRotation not implemented\n" << std::endl;
+  return 0;
+}
